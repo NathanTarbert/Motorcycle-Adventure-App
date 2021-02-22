cc3939a4f585943e15dc2997e59f652caab70dd0
Nathan Tarbert <nate4t@hotmail.comm>
2021-02-21 19:45:38 -0800

    create validation, enroll videos working

--- routes/create.js
+++ routes/create.js
@@ -3,14 +3,39 @@ var router = express.Router();
 var User = require('../models/user');
 const Video = require("../models/video");
 const {handlebars} = require('hbs');
+const { body, validationResult } = require('express-validator');
 /*get the create cube page*/
 router.get('/', function(req, res, next) {
     res.render('create-course', { title: 'Create Course', user: req.user });
 });
 //process the create cube form
-router.post('/', function(req, res, next) {
-    try{
-        // console.log('create video form fired');
+router.post('/', [
+    body('title')
+        .trim()
+        .isLength({min: 4})
+        .withMessage('Title Must Be At Least 4 Characters Long'),
+    body('description')
+        .trim()
+        .isLength({min: 20})
+        .withMessage('Description Must Be At Least 20 Characters'),
+    body('imageUrl')
+        .trim()
+        .isURL({ protocols: ['http', 'https'], require_protocol: true})
+        .withMessage('URL Must Start With HTTP or HTTPS'),
+], async (req, res, next) => {
+    try {
+        var displayErr;
+        const errors = validationResult(req);
+        console.log(errors);
+        if(!errors.isEmpty()){
+            errors.array().forEach(error => {
+                displayErr = error.msg;
+                // console.log(displayErr);       
+            });
+            res.render('create-course', {errors: errors.array()});
+            return;
+        }
+        console.log('create video form fired');
         // console.log(req.body);
         let data = req.body;
         let status = data.isPublic; //status = is video public or not via checkbox
@@ -20,12 +45,13 @@ router.post('/', function(req, res, next) {
                 title: data.title,
                 description: data.description,
                 imageUrl: data.imageUrl,
+                creator: req.user._id,
                 isPublic: true,
                 createdAt: new Date()
             });
             video.save()
             .then((response) => {
-                // console.log(response);
+                console.log(response);
                 res.redirect('/home');
             });
         }else{
@@ -38,12 +64,11 @@ router.post('/', function(req, res, next) {
             });
             video.save()
             .then((response) => {
-                // console.log(response);
+                console.log(response);
                 res.redirect('/home');
             });
         }
-    }catch(error){
-        console.log(error);
-    }
-});
+    }catch(err) {
+        console.log(err);
+    }});
 module.exports = router;
